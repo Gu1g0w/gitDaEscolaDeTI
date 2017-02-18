ca446ff549ba6635ffcbc8ea7544626b78778ab6
Guilherme Oliveira <Guilherme Oliveira>
2017-02-17 22:06:40 -0200

    Adicionando altura a pessoa

--- pessoa.java
+++ pessoa.java
@@ -1,10 +1,12 @@
 public class Pessoa {
 	private String nome;
 	private int idade;
+	private double altura;
 
-	public Pessoa(String nome, int idade){
+	public Pessoa(String nome, int idade, double altura){
 		this.nome = nome;
 		this.idade = idade;
+		this.altura = altura;
 	}
 
 	public String getNome(){
@@ -13,6 +15,9 @@ public class Pessoa {
 
 	public String getIdade(){
 		return idade;
+	}
+	public String getAltura(){
+		return altura;
 	}
 
 	
